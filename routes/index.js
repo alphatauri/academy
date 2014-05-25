318e6fda7f7d3775d8b36406aa006e26e3a3dcd5
Diego Atayra <diego@cycnova.com.ar>
2014-05-25 01:06:19 -0300

    Agregado los scripts y css para permitir el uso de ngGrid pero se rompio angular, ya no aparece las directivas

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,12 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res) {
-  res.render('index', { title: 'Express' });
+	res.render('index');
 });
+
+router.get('/docente', function(req, res){
+	console.log('buscando docente');
+	res.render('./docente/index', {title:'Docentes', subtitle:'administración'});
+})
 
 module.exports = router;
